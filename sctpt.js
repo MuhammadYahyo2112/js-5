ee62cb838f247dd99c854ddd558c32a151cae60c
MuhammadYahyo2112 <yahyo0121@gmail.com>
2025-04-12 18:13:52 +0300

    Add files via upload

---
+++ sctpt.js
@@ -0,0 +1,18 @@
+ let ism =prompt( ' ismingizni  kiriting ' )
+ let id = prompt( ' Spacedagi idingizni kiriting ' )
+ let posportingiz = prompt( ' Spacedagi posportingizni kiriting' )
+
+
+ const obj = { 
+    names: ism,
+    id:  id ,
+    posportingiz: posportingiz ,
+ }
+
+
+console.log(obj);
+
+
+
+
+
